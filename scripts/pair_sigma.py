d1d4517a366743b13b93c618b1016820e964bc59
carmeloevoli <carmelo.evoli@gssi.it>
2025-11-24 21:56:57 +0100

    working on pair absorption

--- scripts/pair_sigma.py
+++ scripts/pair_sigma.py
@@ -9,7 +9,8 @@ import math
 sigma_T = 1.
 
 def beta_star(x):
-    return np.sqrt(...)
+    # x = s / 4 m_e c^4
+    return np.sqrt(1. - 1. / x)
 
 def sigma_pp(x):
     bs = beta_star(x)
@@ -22,23 +23,24 @@ fig = plt.figure(figsize=(11.5, 8.0))
 ax = fig.add_subplot(111)
 
 # Axes limits
-ax.set_xlim(1e-3, 1e3)
-ax.set_ylim(1e-3, 1e1)
+ax.set_xlim(0.4, 1e3)
+ax.set_ylim(1e-3, 0.4)
 
-x = np.logspace(-4, 4, 1000) # E_gamma \epsilon
-s_KN = sigma_KN(x)
-x_hi = np.logspace(0, 4, 1000)
-s_hi = sigma_high(x_hi)
+x = np.logspace(0, 4, 10000)
+y = sigma_pp(x)
 
-ax.loglog(x, s_KN, lw=5, label=r" ")
-ax.loglog(x_hi, s_hi, lw=3, ls=':', label=r" ")
+id = (y == max(y))
+print(x[id], y[id])
 
-ax.hlines(1., 1e-4, 1e4, ls=':', color='tab:gray')
-#ax.text(0.19, 1e-3, r'$\nu_{\rm max}$', rotation=90, color='tab:gray')
-ax.set_xlabel(r'$x \,=\, \epsilon/m_e c^2$')
-ax.set_ylabel(r'$\sigma_{\rm KN}(x) \,/\, \sigma_{\rm T}$')
-#ax.legend(loc="lower left")
-#ax.grid(True, which="both")
+ax.loglog(x, y, lw=5, label=r" ")
 
-out_path = "sigma_pair.pdf"
+ax.hlines(0.26, 0.1, 1e3, ls=':', color='tab:gray')
+ax.vlines(2., 1e-3, 1, ls=':', color='tab:gray')
+##ax.text(0.19, 1e-3, r'$\nu_{\rm max}$', rotation=90, color='tab:gray')
+ax.set_xlabel(r'$x \,=\, s / 4 m_e^2 c^4$')
+ax.set_ylabel(r'$\sigma_{\gamma\gamma}(x) \,/\, \sigma_{\rm T}$')
+##ax.legend(loc="lower left")
+##ax.grid(True, which="both")
+
+out_path = "pair_sigma.pdf"
 plt.savefig(out_path, bbox_inches='tight', dpi=200)
